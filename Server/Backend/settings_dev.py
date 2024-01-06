3a717cff53afb60bbd0f6a4f16dbc9c17679c806
lai hung wei <128201642+s990093@users.noreply.github.com>
2024-01-06 20:53:35 +0800

    fix

--- Server/Backend/settings_dev.py
+++ Server/Backend/settings_dev.py
@@ -32,3 +32,22 @@ current_db_name = connections['default'].settings_dict['ENGINE']
 
 
 PrinterTool.print_green(f"use db {current_db_name}")
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        # TODO: 需要改
+        "LOCATION": "redis://redis:6379/1",  # 主机名为服务名 "redis"
+        # "LOCATION": "redis://0.0.0.0:6379/1",  # 主机名为服务名 "redis"
+        "OPTIONS": {
+            "SOCKET_CONNECT_TIMEOUT": 5,  # 連接超時（秒為單位）
+            "SOCKET_TIMEOUT": 5,  # Socket 超時（秒為單位）
+            "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",  # 使用 Zlib 壓縮機制
+            "IGNORE_EXCEPTIONS": True,  # 忽略快取例外
+            # redis客户端类
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+
+        }
+    }
+}
