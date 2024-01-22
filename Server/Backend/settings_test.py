2e80619112667961d3bc1b26883607126e02fd6a
lai hung wei <128201642+s990093@users.noreply.github.com>
2024-01-23 01:19:25 +0800

    fix: test redis

--- Server/Backend/settings_test.py
+++ Server/Backend/settings_test.py
@@ -31,7 +31,7 @@ CACHES = {
         # "LOCATION": "redis://redis:6379/1",  # 主机名为服务名 "redis"
         "LOCATION": "redis://127.0.0.1:6379/1",  # 主机名为服务名 "redis"
 
-        "LOCATION": "redis://0.0.0.0:6379/1",  # 主机名为服务名 "redis"
+        # "LOCATION": "redis://0.0.0.0:6379/1",  # 主机名为服务名 "redis"
         "OPTIONS": {
             "SOCKET_CONNECT_TIMEOUT": 5,  # 連接超時（秒為單位）
             "SOCKET_TIMEOUT": 5,  # Socket 超時（秒為單位）
