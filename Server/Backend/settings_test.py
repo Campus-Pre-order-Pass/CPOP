881b40fefa6de0b1941096bf543fc61c5a699a30
lai hung wei <128201642+s990093@users.noreply.github.com>
2024-01-23 01:17:54 +0800

    fix

--- Server/Backend/settings_test.py
+++ Server/Backend/settings_test.py
@@ -29,6 +29,8 @@ CACHES = {
         'BACKEND': 'django_redis.cache.RedisCache',
         # TODO: 需要改
         # "LOCATION": "redis://redis:6379/1",  # 主机名为服务名 "redis"
+        "LOCATION": "redis://127.0.0.1:6379/1",  # 主机名为服务名 "redis"
+
         "LOCATION": "redis://0.0.0.0:6379/1",  # 主机名为服务名 "redis"
         "OPTIONS": {
             "SOCKET_CONNECT_TIMEOUT": 5,  # 連接超時（秒為單位）
